4bdc94f8ac56f5ff7b43c756eb7ababa5e0d7c13
Stephen Sitton <stephensitton@gmail.com>
2021-12-03 15:25:40 +0000

    Add files via upload

--- advent-2021.com/day2/dive_test.go
+++ advent-2021.com/day2/dive_test.go
@@ -5,19 +5,19 @@ import (
 )
 
 func TestDive(t *testing.T) {
-	if dive("input1.txt") != 150 {
+	if Dive("input1.txt") != 150 {
 		t.Fatal()
 	}
-	if dive("input.txt") != 1855814 {
+	if Dive("input.txt") != 1855814 {
 		t.Fatal()
 	}
 }
 
 func TestDive_Part2(t *testing.T) {
-	if dive_Part2("input1.txt") != 900 {
+	if Dive_Part2("input1.txt") != 900 {
 		t.Fatal()
 	}
-	if dive_Part2("input.txt") != 1845455714 {
+	if Dive_Part2("input.txt") != 1845455714 {
 		t.Fatal()
 	}
 }
