9111938c34d0fe6596e802ef766fb10ec4238d7d
Stephen Sitton <stephensitton@gmail.com>
2021-12-03 15:25:12 +0000

    Add files via upload

--- advent-2021.com/day1/sonar_sweep_test.go
+++ advent-2021.com/day1/sonar_sweep_test.go
@@ -5,19 +5,19 @@ import (
 )
 
 func TestSonarSweep(t *testing.T) {
-	if sonarSweep("report1.txt") != 7 {
+	if SonarSweep("report1.txt") != 7 {
 		t.Fatal()
 	}
-	if sonarSweep("report.txt") != 1266 {
+	if SonarSweep("report.txt") != 1266 {
 		t.Fatal()
 	}
 }
 
 func TestSonarSweep_Part2(t *testing.T) {
-	if sonarSweep_Part2("report1.txt") != 5 {
+	if SonarSweep_Part2("report1.txt") != 5 {
 		t.Fatal()
 	}
-	if sonarSweep_Part2("report.txt") != 1217 {
+	if SonarSweep_Part2("report.txt") != 1217 {
 		t.Fatal()
 	}
 }
